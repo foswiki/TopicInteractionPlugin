c327cf3235de04e458f5fac554c2f5d7b734b17d
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-01-31 07:34:15 +0000

    Item12373: using ATTACHLINKBOX to control the default state of the "create link" checkbox
    git-svn-id: http://svn.foswiki.org/trunk/TopicInteractionPlugin@16499 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TopicInteractionPlugin.pm
+++ lib/Foswiki/Plugins/TopicInteractionPlugin.pm
@@ -18,8 +18,8 @@ package Foswiki::Plugins::TopicInteractionPlugin;
 use strict;
 use warnings;
 
-our $VERSION = '3.22';
-our $RELEASE = '3.22';
+our $VERSION = '3.23';
+our $RELEASE = '3.23';
 our $SHORTDESCRIPTION = 'Improved interaction with attachments and !DataForms';
 our $NO_PREFS_IN_TOPIC = 1;
 
