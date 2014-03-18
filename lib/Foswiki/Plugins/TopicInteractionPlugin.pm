03b5b60946a72cf2ea7f7141796e83af0486c690
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2014-03-18 14:49:27 +0000

    Item12766: render correct form data when displaying an old revision
    git-svn-id: http://svn.foswiki.org/trunk/TopicInteractionPlugin@17404 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TopicInteractionPlugin.pm
+++ lib/Foswiki/Plugins/TopicInteractionPlugin.pm
@@ -18,8 +18,8 @@ package Foswiki::Plugins::TopicInteractionPlugin;
 use strict;
 use warnings;
 
-our $VERSION = '3.50';
-our $RELEASE = '3.50';
+our $VERSION = '3.51';
+our $RELEASE = '3.51';
 our $SHORTDESCRIPTION = 'Improved interaction with attachments and !DataForms';
 our $NO_PREFS_IN_TOPIC = 1;
 
