17b1f5a375bbf65b1860a7683a8e86b1531eb43e
MichaelDaum <daum@michaeldaumconsulting.com>
2022-06-29 18:19:02 +0200

    Item15152: preparing release

--- lib/Foswiki/Plugins/TopicInteractionPlugin.pm
+++ lib/Foswiki/Plugins/TopicInteractionPlugin.pm
@@ -18,8 +18,8 @@ package Foswiki::Plugins::TopicInteractionPlugin;
 use strict;
 use warnings;
 
-our $VERSION = '9.10';
-our $RELEASE = '09 Jun 2022';
+our $VERSION = '9.20';
+our $RELEASE = '29 Jun 2022';
 our $SHORTDESCRIPTION = 'Improved interaction with attachments and !DataForms';
 our $NO_PREFS_IN_TOPIC = 1;
 our $core;
